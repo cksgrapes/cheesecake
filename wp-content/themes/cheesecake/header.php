4cefd9a894693d02a23cd73ffca9255ed93a375d
Ai Shimizu <cksgrapes@gmail.com>
2017-10-08 09:24:10 +0900

    [add] cheesecake テーマファイル追加

---
+++ wp-content/themes/cheesecake/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>七瀬とマシロの愛の練習日記</title>
+<?php wp_head(); ?>
+</head>
+<body>
---
+++ wp-content/themes/cheesecake/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+	<h1>七瀬とマシロの愛の練習日記</h1>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+
+<?php endwhile; ?>
+
+
+<?php get_footer(); ?>
